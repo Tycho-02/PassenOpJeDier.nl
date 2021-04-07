27b9bbeca7c0778c0491e7c68320eef242b08626
Tycho van Veen <tycho05.vanveen@gmail.com>
2021-04-07 22:30:38 +0200

    created models

---
+++ app/Models/Oppasser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Oppasser extends Model
+{
+    protected $table = 'oppasser';
+}
---
+++ app/Models/Oppasvraag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Oppasvraag extends Model
+{
+    protected $table = 'oppasvraag';
+}
