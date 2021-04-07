e325be2ea1248486146d76effeaaa9f52f2fc78d
Tycho van Veen <tycho05.vanveen@gmail.com>
2021-04-07 21:10:36 +0200

    Added first seeders

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "Tycho van Veen",
+            'email' => "mail@tychovanveen.nl",
+            'password' => bcrypt('12345'),
+            'role' => "Admin",
+            'geboortedatum' => "2002-02-03",
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Mitch van Veen",
+            'email' => "mitch@mail.nl",
+            'password' => bcrypt('12345'),
+            'isHuisdier' => true,
+            'geboortedatum' => "2005-01-03",
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Hans van Veen",
+            'email' => "hans@mail.nl",
+            'password' => bcrypt('12345'),
+            'isOppasser' => true,
+            'geboortedatum' => "1968-05-04",
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Esther van Veen",
+            'email' => "esther@mail.nl",
+            'password' => bcrypt('12345'),
+            'isOppasser' => true,
+            'isHuisdier' => true,
+            'geboortedatum' => "1972-12-12",
+        ]);
+    }
+}
